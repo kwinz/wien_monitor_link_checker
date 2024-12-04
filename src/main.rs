a1965ea0489d2968e53a859528837437ce0c0bad
Markus Krainz <ldm@gmx.at>
2024-12-04 23:01:09 +0100

    Can make HTTPS requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let url = "https://www.wien.gv.at/regierungsabkommen2020/regierungsmonitor/?kategorien=&umsetzungsstand=&page=10";
+
+    let response: Result<reqwest::Response, Error> = reqwest::get(url).await;
+
+    match response {
+        Ok(resp) => {
+            let status = resp.status();
+            let body = resp.text().await?;
+
+            println!("HTTP Status Code: {}", status);
+            println!("Response Body:\n{}", body);
+        }
+        Err(err) => {
+            eprintln!("Failed to make request: {}", err);
+        }
+    }
+
+    Ok(())
 }
