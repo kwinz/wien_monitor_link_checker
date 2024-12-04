d853dccb886f0bf4512c903869f03f67c0e76cc4
Markus Krainz <ldm@gmx.at>
2024-12-04 23:18:17 +0100

    can find h3 headlines in response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use regex::Regex;
 use reqwest::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let url = "https://www.wien.gv.at/regierungsabkommen2020/regierungsmonitor/?kategorien=&umsetzungsstand=&page=10";
+    let pattern = r"<h3>"; // Matches HTTP/HTTPS URLs
 
     let response: Result<reqwest::Response, Error> = reqwest::get(url).await;
 
@@ -12,7 +14,15 @@ async fn main() -> Result<(), Error> {
             let body = resp.text().await?;
 
             println!("HTTP Status Code: {}", status);
-            println!("Response Body:\n{}", body);
+            //println!("Response Body:\n{}", body);
+
+            // Compile the regex
+            let re = Regex::new(pattern).unwrap();
+
+            // Find and print all matches
+            for cap in re.find_iter(&body) {
+                println!("Found match: {}", cap.as_str());
+            }
         }
         Err(err) => {
             eprintln!("Failed to make request: {}", err);
