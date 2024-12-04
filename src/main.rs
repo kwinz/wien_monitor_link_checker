4f92cc94aa90316069c5c782a53bbc42dfc51b26
Markus Krainz <ldm@gmx.at>
2024-12-04 23:40:20 +0100

    minior changes

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
 use regex::Regex;
-use reqwest::Error;
+use reqwest::{Error, Response};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let url = "https://www.wien.gv.at/regierungsabkommen2020/regierungsmonitor/?kategorien=&umsetzungsstand=&page=10";
-    let pattern = r"<h3>"; // Matches HTTP/HTTPS URLs
+    let pattern = r"<h3>.*</h3>"; // Matches HTTP/HTTPS URLs
 
     let response: Result<reqwest::Response, Error> = reqwest::get(url).await;
 
-    match response {
-        Ok(resp) => {
-            let status = resp.status();
-            let body = resp.text().await?;
+    if let Ok(resp) = response {
+        let status = resp.status();
+        let body = resp.text().await?;
 
-            println!("HTTP Status Code: {}", status);
-            //println!("Response Body:\n{}", body);
+        println!("HTTP Status Code: {}", status);
+        //println!("Response Body:\n{}", body);
 
-            // Compile the regex
-            let re = Regex::new(pattern).unwrap();
+        let re = Regex::new(pattern).unwrap();
 
-            // Find and print all matches
-            for cap in re.find_iter(&body) {
-                println!("Found match: {}", cap.as_str());
-            }
+        for cap in re.find_iter(&body) {
+            println!("Found match: {}, {:?}", cap.as_str(), cap);
         }
-        Err(err) => {
-            eprintln!("Failed to make request: {}", err);
-        }
+    } else if let Err(err) = response {
+        eprintln!("Failed to make request: {}", err);
     }
 
     Ok(())
