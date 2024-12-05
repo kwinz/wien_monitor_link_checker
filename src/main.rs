a8f2830e74dac39a89f99ab98a4af344fc7f7c21
Markus Krainz <ldm@gmx.at>
2024-12-05 12:24:43 +0100

    Can request all 9 pages

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,79 @@ use reqwest::{Error, Response};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let url = "https://www.wien.gv.at/regierungsabkommen2020/regierungsmonitor/?kategorien=&umsetzungsstand=&page=10";
+    let regierungsabkommen_url = "https://www.wien.gv.at/regierungsabkommen2020/regierungsmonitor/";
     let uncompiled_h3_regex = r"<h3>(.*)</h3>"; // Matches HTTP/HTTPS URLs
     let uncompiled_link_regex = "<a href=\"(.*)\">(.*)</a>";
 
-    let response: Result<reqwest::Response, Error> = reqwest::get(url).await;
+    let mut urls = vec![];
 
-    if let Ok(resp) = response {
-        let status = resp.status();
-        let body = resp.text().await?;
+    for i in 1..10 {
+        let page_url = if i == 1 {
+            regierungsabkommen_url.to_string()
+        } else {
+            format!("{regierungsabkommen_url}seite-{i}")
+        };
+        println!("Requesting {}", page_url);
+        let response: Result<reqwest::Response, Error> = reqwest::get(page_url).await;
 
-        println!("HTTP Status Code: {}", status);
-        //println!("Response Body:\n{}", body);
+        if let Ok(resp) = response {
+            let status = resp.status();
+            let body = resp.text().await?;
 
-        let h3_regex = Regex::new(uncompiled_h3_regex).unwrap();
-        let link_regex = Regex::new(uncompiled_link_regex).unwrap();
+            println!("HTTP Status Code: {}", status);
+            //println!("Response Body:\n{}", body);
 
-        let mut h3_groups_iter = h3_regex.captures_iter(&body).peekable();
+            let h3_regex = Regex::new(uncompiled_h3_regex).unwrap();
+            let link_regex = Regex::new(uncompiled_link_regex).unwrap();
 
-        let mut sections = vec![];
+            let mut h3_groups_iter = h3_regex.captures_iter(&body).peekable();
 
-        while let Some(new_h3_match) = h3_groups_iter.next() {
-            let h3 = new_h3_match.get(1).unwrap().as_str();
-            let text_start = new_h3_match.get(0).unwrap().end();
+            let mut sections = vec![];
 
-            let text_end = if h3_groups_iter.peek().is_none() {
-                body.len()
-            } else {
-                h3_groups_iter.peek().unwrap().get(0).unwrap().start()
-            };
+            while let Some(new_h3_match) = h3_groups_iter.next() {
+                let h3 = new_h3_match.get(1).unwrap().as_str();
+                let text_start = new_h3_match.get(0).unwrap().end();
 
-            sections.push((h3, text_start, text_end));
+                let text_end = if h3_groups_iter.peek().is_none() {
+                    body.len()
+                } else {
+                    h3_groups_iter.peek().unwrap().get(0).unwrap().start()
+                };
+
+                sections.push((h3, text_start, text_end));
+            }
+
+            //println!("Found matchs {:?}", sections);
+
+            for (h3, start, end) in sections {
+                let all_text_of_h3 = &body[start..end];
+
+                let links: Vec<_> = link_regex
+                    .captures_iter(all_text_of_h3)
+                    .map(|link_match| {
+                        //get(0) is the full capture
+                        let url = link_match.get(1).expect("url missing").as_str();
+                        let name = link_match.get(2).expect("name missing").as_str();
+
+                        //if url.to_string().contains("Grätzelinitiative") {
+                        //    print!("{name} : {url}");
+                        //}
+
+                        urls.push(url.to_string());
+                        format!("{name} : {url}")
+                    })
+                    .collect();
+
+                println!("Match {} {:?}\n", h3, links);
+            }
+        } else if let Err(err) = response {
+            eprintln!("Failed to make request: {}", err);
         }
-
-        //println!("Found matchs {:?}", sections);
-
-        for (h3, start, end) in sections {
-            let all_text_of_h3 = &body[start..end];
-
-            let links: Vec<_> = link_regex
-                .captures_iter(all_text_of_h3)
-                .map(|link_match| {
-                    //get(0) is the full capture
-                    let url = link_match.get(1).expect("url missing").as_str();
-                    let name = link_match.get(2).expect("name missing").as_str();
-                    format!("{name} : {url}")
-                })
-                .collect();
-
-            println!("Match {} {:?}\n", h3, links);
-        }
-    } else if let Err(err) = response {
-        eprintln!("Failed to make request: {}", err);
     }
+
+    urls.sort_unstable();
+
+    println!("Urls {:?}\n", urls);
 
     Ok(())
 }
